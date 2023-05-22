cbfe8b70782d4da0b537f2cc15b45f6c26eadf29
EmanuelaPau <uapame@gmail.com>
2023-05-22 15:06:38 +0200

    Add promt to ask a word

---
+++ script/my_script.js
@@ -0,0 +1,18 @@
+// Palidroma
+// Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma
+// Pari e Dispari
+// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+// Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+// Dichiariamo chi ha vinto.
+
+// PALINDROMA 
+
+// Faccio un prompt e chiedo una parola all'utente. 
+// Verifico la lunghezza della stringa 
+// faccio un contatore il cui massimo e' la lunghezza della stringa
+// creo una nuova stringa con usando il contatore al contrario?? forse posso fare un array?
+// verifico se la prima stringa e' uguale alla seconda
+
+let userWord = prompt("tell me a word!")
