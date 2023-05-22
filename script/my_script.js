4ebe5f1805cbe92422d9f94414bc1f88bf5ba8d0
EmanuelaPau <uapame@gmail.com>
2023-05-22 15:55:33 +0200

    Add prompt

--- script/my_script.js
+++ script/my_script.js
@@ -6,38 +6,41 @@
 // creo una nuova stringa con usando il contatore al contrario?? forse posso fare un array?
 // verifico se la prima stringa e' uguale alla seconda
 
-let userWord = prompt("tell me a word!");
-console.log(userWord)
+// let userWord = prompt("tell me a word!");
+// console.log(userWord)
 
-/**
- * Function that tells you if the string is palindrome
- * @param yourString The user word
- */
+// /**
+//  * Function that tells you if the string is palindrome
+//  * @param yourString The user word
+//  */
 
-function isStringPalindrome(yourString) {
-    let reverseString = "";
+// function isStringPalindrome(yourString) {
+//     let reverseString = "";
 
-    // yourString = "ciccio"
-    for (let i = yourString.length - 1; i >= 0; i--) {
-        reverseString = reverseString + yourString[i];
-    }
+//     // yourString = "ciccio"
+//     for (let i = yourString.length - 1; i >= 0; i--) {
+//         reverseString = reverseString + yourString[i];
+//     }
 
-    console.log(reverseString);
+//     console.log(reverseString);
 
-    if (yourString === reverseString) {
-        return true;
-    } return false;
-}
+//     if (yourString === reverseString) {
+//         return true;
+//     } return false;
+// }
 
 
-if (isStringPalindrome(userWord)) {
-    console.log(`${userWord} è palindroma`);
-} else {
-    console.log(`${userWord} non è palindroma`);
-}
+// if (isStringPalindrome(userWord)) {
+//     console.log(`${userWord} è palindroma`);
+// } else {
+//     console.log(`${userWord} non è palindroma`);
+// }
 
 // Pari e Dispari
 // Faccio un prompt dove chiedo un numero pari o dispari 
 // genero un numero random pari o dispari (con una funzione) 
 // faccio una funzione isNumberEven?
 // creo soluzione con console log e if else
+
+let userNumber = parseInt(prompt('tell me a number from 1 to 5'));
+console.log(userNumber);
