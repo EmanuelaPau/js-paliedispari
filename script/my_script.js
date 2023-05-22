1095d4ebc096e65eae52a11838f99b094c4420aa
EmanuelaPau <uapame@gmail.com>
2023-05-22 15:54:06 +0200

    Add structure pari e dispari

--- script/my_script.js
+++ script/my_script.js
@@ -1,12 +1,3 @@
-// Palidroma
-// Chiedere all’utente di inserire una parola
-// Creare una funzione per capire se la parola inserita è palindroma
-// Pari e Dispari
-// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
-// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
-// Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
-// Dichiariamo chi ha vinto.
-
 // PALINDROMA 
 
 // Faccio un prompt e chiedo una parola all'utente. 
@@ -44,3 +35,9 @@ if (isStringPalindrome(userWord)) {
 } else {
     console.log(`${userWord} non è palindroma`);
 }
+
+// Pari e Dispari
+// Faccio un prompt dove chiedo un numero pari o dispari 
+// genero un numero random pari o dispari (con una funzione) 
+// faccio una funzione isNumberEven?
+// creo soluzione con console log e if else
